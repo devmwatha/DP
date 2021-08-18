ca361273729a8f6d7b033f89a174b85324beeb04
Mwatha <francismwatha@gmail.com>
2021-08-19 00:31:41 +0300

    dynamic programming

--- Test/main/knapsack/KnapsackTest.java
+++ Test/main/knapsack/KnapsackTest.java
@@ -5,11 +5,23 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class KnapsackTest {
+    Knapsack knapsack = new Knapsack();
 
     @Test
     void solveKnapsack() {
-        Knapsack knapsack = new Knapsack();
         int profit = knapsack.solveKnapsack(new int[]{1, 6, 10, 16}, new int[]{1, 2, 3, 5}, 7);
         Assertions.assertEquals(profit, profit);
+    }
+
+    @Test
+    void knapSackTopDown() {
+        int maxProfit = knapsack.solveKnapsack(new int[]{1, 6, 10, 16}, new int[]{1, 2, 3, 5}, 6);
+        Assertions.assertEquals(maxProfit, maxProfit);
+    }
+
+    @Test
+    void knapSackBottomUp() {
+        int maxProfit = knapsack.knapSackBottomUp(new int[]{1, 6, 10, 16}, new int[]{1, 2, 3, 5}, 6);
+        Assertions.assertEquals(maxProfit, maxProfit);
     }
 }
--- src/main/knapsack/Knapsack.java
+++ src/main/knapsack/Knapsack.java
@@ -1,9 +1,18 @@
 package main.knapsack;
 
+import java.util.Arrays;
+
 public class Knapsack {
+    public static void main(String[] args) {
+        Knapsack knapsack = new Knapsack();
+        System.out.println(knapsack.solveKnapsack(new int[]{1, 6, 10, 16}, new int[]{1, 2, 3, 5}, 6));
+    }
 
     public int solveKnapsack(int[] profits, int[] weights, int capacity) {
-        return this.knapSackRecursive(profits, weights, capacity, 0);
+        Integer[][] dp = new Integer[profits.length][capacity + 1];
+        return this.knapSackTopDown(dp, profits, weights, capacity, 0);
+
+//        return this.knapSackRecursive(profits, weights, capacity, 0);
     }
 
     private int knapSackRecursive(int[] profits, int[] weights, int capacity, int currentIndex) {
@@ -17,5 +26,54 @@ public class Knapsack {
         profit1 = knapSackRecursive(profits, weights, capacity, currentIndex + 1);
         return Math.max(profit, profit1);
 
+    }
+
+    private int knapSackTopDown(Integer[][] dp, int[] profits, int[] weights, int capacity, int currentIndex) {
+       /*
+       Space complexity O(N*C +N), time complexity O(N*C)
+        */
+
+        if (capacity <= 0 || currentIndex >= profits.length)
+            return 0;
+
+        if (dp[currentIndex][capacity] != null)
+            return dp[currentIndex][capacity];
+
+
+        int profit = 0;
+        if (weights[currentIndex] <= capacity)
+            profit = profits[currentIndex] + knapSackTopDown(dp, profits, weights, capacity - weights[currentIndex], currentIndex + 1);
+
+        int profit2 = knapSackTopDown(dp, profits, weights, capacity, currentIndex + 1);
+
+        dp[currentIndex][capacity] = Math.max(profit, profit2);
+        return dp[currentIndex][capacity];
+    }
+
+    public int knapSackBottomUp(int[] profits, int[] weights, int capacity) {
+        //base case
+        if (capacity <= 0 || profits.length == 0 || weights.length != profits.length)
+            return 0;
+
+        int n = profits.length;
+        int[][] dp = new int[n][capacity + 1];
+        for (int i = 0; i < n; i++)
+            dp[i][0] = 0;
+
+        for (int c = 0; c <= capacity; c++) {
+            if (weights[0] <= c)
+                dp[0][c] = profits[0];
+        }
+
+        for (int i = 1; i < n; i++) {
+            for (int c = 1; c <= capacity; c++) {
+                int profit = 0, profit1 = 0;
+                if (weights[i] <= c)
+                    profit = profits[i] + dp[i - 1][c - weights[i]];
+                profit1 = dp[i - 1][c];
+                dp[i][c] = Math.max(profit, profit1);
+            }
+        }
+        return dp[n - 1][capacity];
     }
 }
