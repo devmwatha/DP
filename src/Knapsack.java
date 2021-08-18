326a18d1849495e348262eb3240e8e399f7a9fe1
Mwatha <francismwatha@gmail.com>
2021-08-18 22:25:45 +0300

    Initial commit

---
+++ src/Knapsack.java
@@ -0,0 +1,19 @@
+public class Knapsack {
+
+    public int solveKnapsack(int[] profits, int[] weights, int capacity) {
+        return this.knapSackRecursive(profits, weights, capacity, 0);
+    }
+
+    private int knapSackRecursive(int[] profits, int[] weights, int capacity, int currentIndex) {
+        //base case
+        if (capacity <= 0 || currentIndex >= profits.length)
+            return 0;
+        int profit = 0, profit1 = 0;
+        if (weights[currentIndex] <= capacity)
+            profit = profits[currentIndex] + knapSackRecursive(profits, weights, capacity - weights[currentIndex], currentIndex + 1);
+
+        profit1 = knapSackRecursive(profits, weights, capacity, currentIndex + 1);
+        return Math.max(profit, profit1);
+
+    }
+}
